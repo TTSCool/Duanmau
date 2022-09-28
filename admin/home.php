6d1d30f33a4f6824de32394eba3c3c89dbbe51f9
TTSCool <vietdainhan1@gmail.com>
2022-09-28 11:57:14 +0700

    update 0.0.7 a work+

--- admin/home.php
+++ admin/home.php
@@ -13,7 +13,7 @@
         </div>
         <div class="content svang">
             <div class="word">
-                <h1>Sản phẩm bán chạy 1</h1>
+                <h1>Sản phẩm bán chạy 2</h1>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis accusantium iure vel est suscipit, quae, dolores distinctio ipsa vero odio fuga modi deleniti saepe maiores blanditiis cupiditate doloremque quam alias aspernatur. Quo quaerat eum quasi vero.
                 </p>
                 <a href="Sanpham.php"><button>Mua ngay</button></a>

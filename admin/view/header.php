cbfe4ad78bc46dc646c34b133b89b8d59010f590
TTSCool <vietdainhan1@gmail.com>
2022-10-04 02:00:49 +0700

    0.1.2

--- admin/view/header.php
+++ admin/view/header.php
@@ -6,21 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../../css/index.css">
 </head>
 <body>
     <div class="container">
-        <div class="name">
-            Cửa hàng trực tuyến Xshop
-        </div>
-        <br>
         <header>
         <nav>
-                <a href="#">Trang chủ</a>
-                <a href="danhmuc/index.php" >Danh mục</a>
-                <a href="sanpham/index.php">Sản phẩm</a>
-                <a href="nguoidung/index.php">Người dùng</a>
-                <a href="binhluan/index.php">Bình luận</a>
+                <div class="logo">
+                    <a href="3"><img src="../../img/Logo.png" alt=""></a>
+                </div>
+                <a href="../view/index.php">Trang chủ</a>
+                <a href="../danhmuc/index.php" >Danh mục</a>
+                <a href="../sanpham/index.php">Sản phẩm</a>
+                <a href="../nguoidung/index.php">Người dùng</a>
+                <a href="../binhluan/index.php">Bình luận</a>
                 <a href="#">Thống kê</a>
         </nav>
         <div class="search"><input type="text"><button><i class="fa-solid fa-magnifying-glass"></i></button> </div>           
